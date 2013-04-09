da646292a736a85ea9ecd4bf837d336a1d5e2518
Ryan Albrecht <ryan@freshbooks.com>
2013-04-09 10:26:18 -0400

    Removed all the folders that sublime will automatically get for me

--- Packages/User/Redmine.py
+++
@@ -1,8 +0,0 @@
-import sublime, sublime_plugin
-
-class ExampleCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # self.view.insert(edit, 0, "Hello, World!")
-        url = 'https://redmine/issues/18487'
-        webbrowser.open(url) #[, new=0[, autoraise=True]]
-        pass
